04015698521782c50ea2a50c16ebe5a5abf012e8
B1ackRedCat <green.vad.co@gmail.com>
2023-04-11 10:47:14 +0300

    Add task1 to Lesson3

---
+++ Lesson_3/Task_1/A.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class A
+    {
+        #region Private fields
+        private int _numbA;
+        private int _numbB;
+        #endregion
+
+        #region Public fields
+        public int NumbA
+        {
+            get { return _numbA; }
+            set { _numbA = value; }
+        }
+        public int NumbB
+        {
+            get { return _numbB; }
+            set { _numbB = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public A(int NumbA, int NumbB)
+        {
+            _numbA = NumbA;
+            _numbB = NumbB;
+        }
+        #endregion
+
+        #region Methods
+        public int CalculateDifference(int NumbA, int NumbB)
+        {
+            return NumbA - NumbB;
+        }
+
+        public double CalculateQuotient(int NumbA, int NumbB)
+        {
+            return (NumbA + NumbB) / ((NumbA - NumbB));
+        }
+
+        public void Print(int NumbA, int NumbB)
+        {
+            Console.WriteLine("Difference calculation: " + CalculateDifference(NumbA, NumbB));
+            Console.WriteLine("Calculation of the quotient: " + CalculateQuotient(NumbA, NumbB));
+        }
+        #endregion
+    }
+}
--- Lesson_3/Task_1/Program.cs
+++ Lesson_3/Task_1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.Write("Enter number a: ");
+            int numbA = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number b: ");
+            int numbB = int.Parse(Console.ReadLine());
+
+            A a = new A(numbA, numbB);
+            a.Print(numbA, numbB);
         }
     }
 }
