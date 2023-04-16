9453d05fcf4e3c419b732820180769d524fb7199
B1ackRedCat <green.vad.co@gmail.com>
2023-04-16 18:38:11 +0300

    add Task_4

---
+++ Lesson_4/Task_4/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length of row: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Enter length of column: ");
+            int column = int.Parse(Console.ReadLine());
+            Console.Write("Enter length of elements: ");
+            int len = int.Parse(Console.ReadLine());
+            Console.WriteLine("1 - random; 2 - manual ");
+            string input = Console.ReadLine();
+            StringArray array1 = new StringArray(row, column, len);
+            StringArray array2 = new StringArray(row, column, len); 
+            switch (input)
+            {
+                case "1":
+                    array1.GenerateArray();
+                    array2.GenerateArray();
+                    break;
+                case "2":
+                    for (int i = 0; i < row; i++)
+                    {
+                        for (int j = 0; j < column; j++)
+                        {
+                            Console.Write($"{i + 1},{j + 1} element of 1 array: ");
+                            array1[i, j] = Console.ReadLine();
+                            Console.Write($"{i + 1},{j + 1} element of 2 array: ");
+                            array2[i, j] = Console.ReadLine();
+                        }
+                    }
+                    break;
+                default:
+                    array1.GenerateArray();
+                    array2.GenerateArray();
+                    break;
+            }
+            Console.WriteLine(array1);
+            Console.WriteLine();
+            Console.WriteLine(array2);
+            Console.WriteLine($"1 array = 2 array: {array1 == array2}");
+        }
+    }
+}
---
+++ Lesson_4/Task_4/StringArray.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task_4
+{
+    internal class StringArray
+    {
+        #region Private fields
+        private string[,] _array;
+        private int _row;
+        private int _column;
+        private int _length;
+        #endregion
+
+        #region Public fields
+        public string this[int row, int column]
+        {
+            get { return _array[row, column]; }
+            set
+            {
+                if (value.Length == _length)
+                    _array[row, column] = value;
+                else
+                    throw new ArgumentException("Invalid length of string.");
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public StringArray(int row, int column, int length)
+        {
+            _array = new string[row, column];
+            _row = row;
+            _column = column;
+            _length = length;
+        }
+        #endregion
+
+        #region Methods
+        public void GenerateArray()
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            char[] stringChars = new char[_length];
+            Random random = new Random();
+            for (int i = 0; i < _row; i++)
+            {
+                for (int j = 0; j < _column; j++)
+                {
+                    for (int k = 0; k < stringChars.Length; k++)
+                    {
+                        stringChars[k] = chars[random.Next(chars.Length)];
+                    }
+                    _array[i, j] = new string(stringChars);
+                }
+            }
+        }
+
+        public static bool operator !=(StringArray stringArray1, StringArray stringArray2)
+        {
+            return !Enumerable.Range(0, stringArray1._array.Rank).All(dimension => stringArray1._array.GetLength(dimension) == stringArray2._array.GetLength(dimension)) && stringArray1._array.Cast<string>().SequenceEqual(stringArray2._array.Cast<string>());
+            /*if (stringArray1._row == stringArray2._row && stringArray1._column == stringArray2._column)
+            {
+                for (int i = 0; i < stringArray1._row; i++)
+                {
+                    for (int j = 0; j < stringArray1._column; j++)
+                    {
+                        if (stringArray1[i, j] == stringArray2[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true; //это способ поэлементного сравнения*/
+        }
+
+        public static bool operator ==(StringArray stringArray1, StringArray stringArray2)
+        {
+            return Enumerable.Range(0, stringArray1._array.Rank).All(dimension => stringArray1._array.GetLength(dimension) == stringArray2._array.GetLength(dimension)) && stringArray1._array.Cast<string>().SequenceEqual(stringArray2._array.Cast<string>());
+            /*if (stringArray1._row == stringArray2._row && stringArray1._column == stringArray2._column)
+            {
+                for (int i = 0; i < stringArray1._row; i++)
+                {
+                    for (int j = 0; j < stringArray1._column; j++)
+                    {
+                        if (stringArray1[i, j] != stringArray2[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true; //это способ поэлементного сравнения*/
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < _row; i++)
+            {
+                for (int j = 0; j < _column; j++)
+                {
+                    sb.Append($"{_array[i, j]} ");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
