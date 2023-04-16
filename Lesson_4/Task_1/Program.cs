01bbaca7f5083c66e6eeb8e97b2f4748dd16fe15
B1ackRedCat <green.vad.co@gmail.com>
2023-04-16 15:43:50 +0300

    add Task_1

---
+++ Lesson_4/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static double min(double a, double b)
+        {
+            return (a < b) ? a : b;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Enter y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Enter z: ");
+            double z = double.Parse(Console.ReadLine());
+            Console.Write("Enter u: ");
+            double u = double.Parse(Console.ReadLine());
+            double minNumb = min(min(x, y), min(z, u));
+            Console.WriteLine($"Minimum number: {minNumb}");
+        }
+    }
+}
