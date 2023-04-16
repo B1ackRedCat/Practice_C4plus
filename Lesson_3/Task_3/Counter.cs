2f7479724eb465ee5b1f4209af61ee3d7a0b6a8e
B1ackRedCat <green.vad.co@gmail.com>
2023-04-16 15:11:05 +0300

    add Task_3

---
+++ Lesson_3/Task_3/Counter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class Counter
+    {
+        private readonly int _maximum;
+        private readonly int _minimum;
+
+        public int Value { private set; get; }
+
+        public Counter(int maximum, int minimum, int counter)
+        {
+            _maximum = maximum;
+            _minimum = minimum;
+            counter = Math.Min(_maximum, counter);
+            counter = Math.Max(_minimum, counter);
+            this.Value = counter;
+        }
+
+        private Counter() 
+        {
+            _maximum = 10;
+            _minimum = 0;
+            this.Value = 5;
+        }
+
+        public void Increase()
+        {
+            var value = this.Value + 1;
+            if (value > _maximum || value < _minimum)
+                return;
+            this.Value++;
+        }
+
+        public void Decrease()
+        {
+            var value = this.Value - 1;
+            if (value > _maximum || value < _minimum)
+                return;
+            this.Value--;
+        }
+    }
+}
---
+++ Lesson_3/Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+
+            Console.WriteLine("Введите минимум для счетчика");
+            int min = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.WriteLine("Введите максимум для счетчика");
+            int max = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.WriteLine("Введите значение для счетчика");
+            int v = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Counter counter = new Counter(max, min, v);
+            Console.WriteLine("Введите + для увеличение и - для уменьшения, иное для выхода");
+
+            do
+            {
+                var c = Console.ReadKey();
+                if (c.KeyChar == '+') counter.Increase();
+                else if (c.KeyChar == '-') counter.Decrease();
+                else break;
+                Console.WriteLine($" => {counter.Value}");
+            } while (true);
+
+            Console.ReadKey();
+        }
+    }
+}
