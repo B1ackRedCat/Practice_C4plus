5dd0e45b030696aa57d31752321149b41f91d5da
B1ackRedCat <green.vad.co@gmail.com>
2023-05-16 16:12:15 +0300

    adding a third task

---
+++ Lesson_8/Task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "данное предложение, содержит запятую. здесь - есть тире. тут ничего нет. в этом (как видите) скобки. тут снова ничего нет.";
+
+            string[] sentences = GetSentencesWithPunctuation(text);
+
+            if (sentences.Length > 0)
+            {
+                Console.WriteLine("Sentences containing punctuation marks:");
+                foreach (string sentence in sentences)
+                    Console.WriteLine(sentence);
+            }
+            else
+                Console.WriteLine("There are no sentences in the text with the specified punctuation marks.");
+
+            Console.ReadLine();
+        }
+
+        static string[] GetSentencesWithPunctuation(string text)
+        {
+            string[] sentences = Regex.Split(text, @"(?<=[.?!])");
+
+            string[] sentencesWithPunctuation = Array.FindAll(sentences, sentence =>
+                Regex.IsMatch(sentence, @"[,;:\-\(\)""']"));
+
+            return sentencesWithPunctuation;
+        }
+    }
+}
