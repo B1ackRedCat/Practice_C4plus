dccc9771fb1548cd734e27c4d0d2d9fd7d57f9be
B1ackRedCat <green.vad.co@gmail.com>
2023-05-17 11:12:31 +0300

    solution

---
+++ Lesson_12/Task_1/Location.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal abstract class Location
+    {
+        private string name;
+        public string Name { get => name; }
+        public Location(string name)
+        {
+            this.name = name;
+        }
+        public virtual int GetPopulationDensity() => 5;
+        public static double GetMinPopulationDensity(Location[] localities)
+        {
+            if (localities != null)
+            {
+                int min = localities[0].GetPopulationDensity();
+                for (int i = 0; i < localities.Length; i++)
+                {
+                    if (min > localities[i].GetPopulationDensity())
+                    {
+                        min = localities[i].GetPopulationDensity();
+
+                    }
+                }
+                return min;
+            }
+            throw new Exception("Array is null");
+        }
+    }
+}
---
+++ Lesson_12/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Location[] localities = new Location[]
+            {
+                new City("Гродно", 10000, 100),
+                new City("Гомель", 10000 ,100),
+                new City ("Минск", 20000, 200),
+                new Village("Свислочь", 200, 4, 4),
+                new Village("Тихиничи", 100, 4, 3)
+            };
+
+            foreach (Location locality in localities)
+            {
+                Console.WriteLine($"Population density: {locality.GetPopulationDensity():.###}");
+            }
+            Console.Write("Lowest population density:");
+            Console.Write(Location.GetMinPopulationDensity(localities));
+        }
+    }
+}
