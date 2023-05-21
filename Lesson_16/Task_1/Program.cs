86f4150ea8b618ea225d07c968134f54a7445aac
B1ackRedCat <green.vad.co@gmail.com>
2023-05-21 12:46:34 +0300

    push BORROWED solution

---
+++ Lesson_16/Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList<string>();
+            list.Add("Grinko");
+            list.Add("Mark");
+            list.Add("37TP");
+            Console.WriteLine($"Amount of elements: {list.Count}");
+            Console.Write($"Enter the element index from 1 to {list.Count}: ");
+            try
+            {
+                int ind = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Element at {ind} position: {list[ind - 1]}");
+            }
+            catch (Exception) { Console.WriteLine("There is no element with this index"); }
+        }
+    }
+}
---
+++ Lesson_16/Task_3/MyDictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public static class MyDictionaryExtensions
+    {
+        public static KeyValuePair<TKey, TValue>[] GetArray<TKey, TValue>(this MyDictionary<TKey, TValue> dictionary)
+        {
+            KeyValuePair<TKey, TValue>[] array = new KeyValuePair<TKey, TValue>[dictionary.Count];
+            int i = 0;
+            foreach (KeyValuePair<TKey, TValue> pair in dictionary)
+            {
+                array[i++] = pair;
+            }
+            return array;
+        }
+    }
+}
