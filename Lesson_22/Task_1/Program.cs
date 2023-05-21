9f663bf72ff8d51e0a788030cef0fa6971623d25
B1ackRedCat <green.vad.co@gmail.com>
2023-05-21 19:14:10 +0300

    push lesson 22

---
+++ Lesson_22/Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task[] tasks = new Task[]
+            {
+                new Task(() => ThreadMethod("1")),
+                new Task(() => ThreadMethod("2")),
+                new Task(() => ThreadMethod("3")),
+                new Task(() => ThreadMethod("4")),
+                new Task(() => ThreadMethod("5"))
+            };
+            foreach (var task in tasks)
+            {
+                task.Start();
+            }
+            Task.WaitAll(tasks);
+            Console.WriteLine("Работа окончена...");
+        }
+
+        static void ThreadMethod(string query)
+        {
+            SingletonDatabase sd = SingletonDatabase.GetInstance();
+            for (int i = 0; i < 5; i++)
+            {
+                sd.Query($"Запрос от {query} потока. Попытка: {i + 1}");
+            }
+        }
+    }
+}
---
+++ Lesson_22/Task_1/SingletonDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class SingletonDatabase
+    {
+        private static SingletonDatabase instance = null;
+        private static string databaseUrl = "google.com";
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Подключение инициализировано к " + databaseUrl);
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static SingletonDatabase GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SingletonDatabase();
+            }
+            return instance;
+        }
+
+        public void Query(string sql)
+        {
+            Console.WriteLine("Выполняемый запрос: " + sql);
+        }
+    }
+}
